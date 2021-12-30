3c4da08854197baf94d5c3ea4e4b73fdc6ecf948
Mina Husseinpour <92226453+TheBuffMinashi@users.noreply.github.com>
2021-12-30 16:34:45 +0330

    Update MSE.py
    This is a function that calculates mean absolute error (MSE) given a line's slope and interceptor and a set of points (x, y)

--- MSE.py
+++ MSE.py
@@ -11,6 +11,7 @@ def MSE(slope, interceptor, Xs, Ys):
     result = 0.5 * (np.mean(np.power((np.array(Ys) - np.array(y_hats)), 2)))
     return y_hats, result
 
+
 vals = [1.2, 2, [2, 5, -4, -7, 8], [-2, 6, -4, 1, 14]]
 y_hats, result = MSE(vals[0], vals[1], vals[2], vals[3])
 print('Results {:.2f}'.format(result))
